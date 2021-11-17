643aa47a9945627f6d270312bc75d5002fc215bc
Samuel Grigolato <samuel.grigolato@gmail.com>
2021-11-17 19:05:19 -0300

    Remove toda a implementação real

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import { useCallback, useEffect, useState } from 'react';
-import TruffleContract from '@truffle/contract';
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import Web3 from "web3";
-import SCHEMA_CONTRATO from './Crowdfunding.json';
 import './App.css';
 
-const ENDERECO_ROPSTEN = '0x6C19bDcb7aDec160F17E6ed2eeaEAb01411E431e';
-
-const provider = new WalletConnectProvider({
-  infuraId: "b46382ca47364b21b6ed9379fc70f023",
-  chainId: 3 /*ropsten*/
-});
-
 function meu(projeto) {
-  //return window['ethereum'].selectedAddress.toLowerCase() === projeto.solicitante.toLowerCase();
-  return provider.accounts[0].toLowerCase() === projeto.solicitante.toLowerCase();
+  return '0x135Ca03139Cb9CA04C41c052380Bc872298643a8'.toLowerCase() === projeto.solicitante.toLowerCase();
 }
 
 function expirado(projeto) {
@@ -25,377 +13,73 @@ function arrecadado(projeto) {
   return projeto.weiObtido >= projeto.weiSolicitado;
 }
 
-// const ENDERECO_CONTRATO = '0x469a6540fb77b118e38a38C8881375101107811c';
-// const Crowdfunding = TruffleContract(SCHEMA_CONTRATO);
-
-async function buscarProjetos() {
-  // const contrato = await Crowdfunding.at(ENDERECO_CONTRATO);
-  // const projetos = await contrato.todosOsProjetos();
-
-  const web3 = new Web3(provider);
-  const projetosEncoded = await web3.eth.call({
-    from: provider.accounts[0],
-    to: ENDERECO_ROPSTEN,
-    data: web3.eth.abi.encodeFunctionCall(
-      {
-        "inputs": [],
-        "name": "todosOsProjetos",
-        "outputs": [
-          {
-            "components": [
-              {
-                "internalType": "address",
-                "name": "solicitante",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "weiSolicitado",
-                "type": "uint256"
-              },
-              {
-                "internalType": "uint256",
-                "name": "weiObtido",
-                "type": "uint256"
-              },
-              {
-                "internalType": "uint256",
-                "name": "timestampLimite",
-                "type": "uint256"
-              },
-              {
-                "internalType": "bool",
-                "name": "ativo",
-                "type": "bool"
-              }
-            ],
-            "internalType": "struct Crowdfunding.Projeto[]",
-            "name": "",
-            "type": "tuple[]"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      []
-    )
-  });
-  const projetos = web3.eth.abi.decodeParameter(
-    {
-      "components": [
-        {
-          "internalType": "address",
-          "name": "solicitante",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "weiSolicitado",
-          "type": "uint256"
-        },
-        {
-          "internalType": "uint256",
-          "name": "weiObtido",
-          "type": "uint256"
-        },
-        {
-          "internalType": "uint256",
-          "name": "timestampLimite",
-          "type": "uint256"
-        },
-        {
-          "internalType": "bool",
-          "name": "ativo",
-          "type": "bool"
-        }
-      ],
-      "internalType": "struct Crowdfunding.Projeto[]",
-      "name": "",
-      "type": "tuple[]"
-    },
-    projetosEncoded
-  );
-  return projetos.map(projeto => ({
-    ativo: projeto.ativo,
-    solicitante: projeto.solicitante,
-    timestampLimite: parseInt(projeto.timestampLimite),
-    weiObtido: parseInt(projeto.weiObtido),
-    weiSolicitado: parseInt(projeto.weiSolicitado)
-  }));
-}
-
 function App() {
 
-  const [ state, setState ] = useState({ status: 'inicializando' });
+  // const [ state, setState ] = useState({ status: 'inicializando' });
   // const [ state, setState ] = useState({ status: 'processando' });
   // const [ state, setState ] = useState({ status: 'carregando' });
   // const [ state, setState ] = useState({
   //   status: 'erro',
   //   mensagem: 'Algo muito ruim aconteceu!'
   // });
-  // const [ state, setState ] = useState({
-  //   status: 'pronto',
-  //   projetos: [
-  //     {
-  //       solicitante: '0x135Ca03139Cb9CA04C41c052380Bc872298643a8',
-  //       timestampLimite: (new Date().getTime() / 1000) + 100,
-  //       ativo: true,
-  //       weiObtido: 0,
-  //       weiSolicitado: 100
-  //     },
-  //     {
-  //       solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
-  //       timestampLimite: (new Date().getTime() / 1000) + 100,
-  //       ativo: true,
-  //       weiObtido: 0,
-  //       weiSolicitado: 100
-  //     },
-  //     {
-  //       solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
-  //       timestampLimite: (new Date().getTime() / 1000) + 10000,
-  //       ativo: false,
-  //       weiObtido: 500,
-  //       weiSolicitado: 10000000
-  //     },
-  //     {
-  //       solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
-  //       timestampLimite: (new Date().getTime() / 1000) - 300,
-  //       ativo: false,
-  //       weiObtido: 1000,
-  //       weiSolicitado: 9900
-  //     },
-  //     {
-  //       solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
-  //       timestampLimite: (new Date().getTime() / 1000) + 100,
-  //       ativo: true,
-  //       weiObtido: 1000,
-  //       weiSolicitado: 900
-  //     },
-  //     {
-  //       solicitante: '0x135Ca03139Cb9CA04C41c052380Bc872298643a8',
-  //       timestampLimite: (new Date().getTime() / 1000) + 100,
-  //       ativo: true,
-  //       weiObtido: 1000,
-  //       weiSolicitado: 900
-  //     }
-  //   ]
-  // });
-
-  useEffect(() => {
-    // if (!window['ethereum']) {
-    //   setState({ status: 'erro', mensagem: 'Habilite/instale a extensão MetaMask e tente novamente' });
-    //   return;
-    // }
-    (async () => {
-      try {
-
-        await provider.enable();
-        // await window['ethereum'].enable();
-
-        // setState({ status: 'carregando' });
-
-        // Crowdfunding.setProvider(window['ethereum']);
-
-        const projetos = await buscarProjetos();
-        setState({
-          status: 'pronto',
-          projetos
-        });
-
-      } catch (err) {
-        console.error(err);
-        setState({ status: 'erro', mensagem: err.message || err.toString() });
+  const [ state, setState ] = useState({
+    status: 'pronto',
+    projetos: [
+      {
+        solicitante: '0x135Ca03139Cb9CA04C41c052380Bc872298643a8',
+        timestampLimite: (new Date().getTime() / 1000) + 100,
+        ativo: true,
+        weiObtido: 0,
+        weiSolicitado: 100
+      },
+      {
+        solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
+        timestampLimite: (new Date().getTime() / 1000) + 100,
+        ativo: true,
+        weiObtido: 0,
+        weiSolicitado: 100
+      },
+      {
+        solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
+        timestampLimite: (new Date().getTime() / 1000) + 10000,
+        ativo: false,
+        weiObtido: 500,
+        weiSolicitado: 10000000
+      },
+      {
+        solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
+        timestampLimite: (new Date().getTime() / 1000) - 300,
+        ativo: false,
+        weiObtido: 1000,
+        weiSolicitado: 9900
+      },
+      {
+        solicitante: '0x234Ca03139Cb9CA04C41c052380Bc872298643e3',
+        timestampLimite: (new Date().getTime() / 1000) + 100,
+        ativo: true,
+        weiObtido: 1000,
+        weiSolicitado: 900
+      },
+      {
+        solicitante: '0x135Ca03139Cb9CA04C41c052380Bc872298643a8',
+        timestampLimite: (new Date().getTime() / 1000) + 100,
+        ativo: true,
+        weiObtido: 1000,
+        weiSolicitado: 900
       }
-    })();
-  }, []);
+    ]
+  });
 
   const solicitar = useCallback(async (wei, limiteEmSegundos) => {
-    try {
-
-      setState({ status: 'processando' });
-      
-      const web3 = new Web3(provider);
-      await web3.eth.sendTransaction({
-        from: provider.accounts[0],
-        to: ENDERECO_ROPSTEN,
-        data: web3.eth.abi.encodeFunctionCall(
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "_wei",
-                "type": "uint256"
-              },
-              {
-                "internalType": "uint256",
-                "name": "limiteEmSegundos",
-                "type": "uint256"
-              }
-            ],
-            "name": "solicitar",
-            "outputs": [
-              {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-              }
-            ],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          [wei, limiteEmSegundos]
-        )
-      });
-      // const contrato = await Crowdfunding.at(ENDERECO_CONTRATO);
-      // await contrato.solicitar(wei, limiteEmSegundos, {
-      //   from: window['ethereum'].selectedAddress
-      // });
-
-      setState({ status: 'carregando' });
-      const projetos = await buscarProjetos();
-      setState({
-        status: 'pronto',
-        projetos
-      });
-
-    } catch (err) {
-      console.error(err);
-      setState({ status: 'erro', mensagem: err.message || err.toString() });
-    }
   }, []);
 
   const contribuir = useCallback(async (idProjeto, wei) => {
-    try {
-
-      setState({ status: 'processando' });
-
-      const web3 = new Web3(provider);
-      await web3.eth.sendTransaction({
-        from: provider.accounts[0],
-        to: ENDERECO_ROPSTEN,
-        value: wei,
-        data: web3.eth.abi.encodeFunctionCall(
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "idProjeto",
-                "type": "uint256"
-              }
-            ],
-            "name": "contribuir",
-            "outputs": [],
-            "stateMutability": "payable",
-            "type": "function"
-          },
-          [idProjeto]
-        )
-      });
-      // const contrato = await Crowdfunding.at(ENDERECO_CONTRATO);
-      // await contrato.contribuir(idProjeto, {
-      //   value: wei,
-      //   from: window['ethereum'].selectedAddress
-      // });
-
-      setState({ status: 'carregando' });
-      const projetos = await buscarProjetos();
-      setState({
-        status: 'pronto',
-        projetos
-      });
-
-    } catch (err) {
-      console.error(err);
-      setState({ status: 'erro', mensagem: err.message || err.toString() });
-    }
-  }, []);
-
-  const resgatar = useCallback(async (idProjeto) => {
-    try {
-
-      setState({ status: 'processando' });
-
-      const web3 = new Web3(provider);
-      await web3.eth.sendTransaction({
-        from: provider.accounts[0],
-        to: ENDERECO_ROPSTEN,
-        data: web3.eth.abi.encodeFunctionCall(
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "idProjeto",
-                "type": "uint256"
-              }
-            ],
-            "name": "resgatar",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          [idProjeto]
-        )
-      });
-      // const contrato = await Crowdfunding.at(ENDERECO_CONTRATO);
-      // await contrato.resgatar(idProjeto, {
-      //   from: window['ethereum'].selectedAddress
-      // });
-
-      setState({ status: 'carregando' });
-      const projetos = await buscarProjetos();
-      setState({
-        status: 'pronto',
-        projetos
-      });
-
-    } catch (err) {
-      console.error(err);
-      setState({ status: 'erro', mensagem: err.message || err.toString() });
-    }
   }, []);
 
   const recuperar = useCallback(async (idProjeto) => {
-    try {
+  }, []);
 
-      setState({ status: 'processando' });
-
-      const web3 = new Web3(provider);
-      await web3.eth.sendTransaction({
-        from: provider.accounts[0],
-        to: ENDERECO_ROPSTEN,
-        data: web3.eth.abi.encodeFunctionCall(
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "idProjeto",
-                "type": "uint256"
-              }
-            ],
-            "name": "recuperarMinhasContribuicoes",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          [idProjeto]
-        )
-      });
-      // const contrato = await Crowdfunding.at(ENDERECO_CONTRATO);
-      // await contrato.recuperarMinhasContribuicoes(idProjeto, {
-      //   from: window['ethereum'].selectedAddress
-      // });
-
-      setState({ status: 'carregando' });
-      const projetos = await buscarProjetos();
-      setState({
-        status: 'pronto',
-        projetos
-      });
-
-    } catch (err) {
-      console.error(err);
-      setState({ status: 'erro', mensagem: err.message || err.toString() });
-    }
+  const resgatar = useCallback(async (idProjeto) => {
   }, []);
 
   return (
